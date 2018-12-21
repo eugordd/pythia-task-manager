db7d634300eae95c646c93187386e8eeead0c9a4
rayjackson <rayjackson132@gmail.com>
2018-12-22 01:13:04 +0600

    Header implemented.

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,17 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style lang="scss">
+    @import url('https://fonts.googleapis.com/css?family=Roboto');
     
+    body {
+        min-width: 320px;
+        font-family: 'Roboto', Arial, Helvetica, sans-serif;
+        margin: 0;
+        padding: 0;
+    }
+
+    *, *::after, *::before {
+        box-sizing: border-box;
+    }
 </style>
---
+++ src/components/app/Global/Header/SearchBar.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="searchbar-wrap">
+        <input type="text" class="searchbar" id="search" v-model="text" @input="changeText()">
+        <i class="searchbar-icon"></i>
+        <label v-if="isEmpty" for="search" class="searchbar-label">Search</label>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SearchBar',
+    data() {
+        return {
+            text: '',
+            isEmpty: true
+        }
+    },
+    methods: {
+        changeText() {
+            this.isEmpty = this.text.length === 0
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    .searchbar {
+        padding: 29px 30px 30px 65px;
+        font-size: 18px;
+        width: 100%;
+        line-height: 21px;
+        border: 0;
+        flex: 1;
+
+        &:focus {
+            outline: 0;
+            & ~ .searchbar-label {
+                display: none;
+            }
+        }
+
+        &-wrap {
+            position: relative;
+            flex: 1;
+            display: flex;
+        }
+
+        &-icon {
+            position: absolute;
+            left: 20px;
+            top: 26px;
+            width: 32px;
+            height: 32px;
+            background-image: url('/images/svg/magnify.svg');
+            background-size: contain;
+        }
+
+        &-label {
+            position: absolute;
+            top: 30px;
+            left: 65px;
+            font-size: 18px;
+            line-height: 21px;
+            color: #b3becb;
+        }
+    }
+        
+</style>
+
