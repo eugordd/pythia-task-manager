d1756f78d51f30181fbc0aaad25e11ebe2f09099
EuGorDD <eugord@Yandex.ru>
2018-12-21 18:18:33 +0300

    fix: scss instead sass

--- src/pages/TasksListPage.vue
+++ src/pages/TasksListPage.vue
@@ -58,48 +58,52 @@ export default {
 }
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
 
-.tasks-list
-    width: 1000px
-    margin: 0 auto
+.tasks-list {
+    width: 1000px;
+    margin: 0 auto;
+}
 
-.tasks
-    display: flex
-    overflow-x: scroll
+.tasks {
+    display: flex;
+    overflow-x: scroll;
 
-    &__column
-        display: flex
-        flex: 0 0 auto
-        flex-direction: column
-        width: 300px
-        margin-right: 40px
+    &__column {
+        display: flex;
+        flex: 0 0 auto;
+        flex-direction: column;
+        width: 300px;
+        margin-right: 40px;
 
-        &-title
-            color: #AAB3BC
-            font-weight: 500
-            font-size: 18px
-            margin-bottom: 10px
+        &-title {
+            color: #AAB3BC;
+            font-weight: 500;
+            font-size: 18px;
+            margin-bottom: 10px;
+        }
+    }
 
-    &__card
-        display: flex
-        flex: 0 0 auto
-        padding: 20px
-        flex-direction: column
-        background: #fff
-        box-shadow: 0px 4px 10px rgba(170, 179, 188, 0.19)
-        border-radius: 5px
-        margin-bottom: 20px
+     &__card {
+        display: flex;
+        flex: 0 0 auto;
+        padding: 20px;
+        flex-direction: column;
+        background: #fff;
+        box-shadow: 0px 4px 10px rgba(170, 179, 188, 0.19);
+        border-radius: 5px;
+        margin-bottom: 20px;
 
-        &-title
-            color: #2E2E2E
-            font-weight: 500
-            font-size: 14px
+         &-title {
+            color: #2E2E2E;
+            font-weight: 500;
+            font-size: 14px;
+         }
 
-        &-description
-            color: #AAB3BC
-            font-size: 12px
-            
-
-    
+        &-description {
+            color: #AAB3BC;
+            font-size: 12px;
+        }
+     }
+}
 </style>
