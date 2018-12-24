b5658a36beef3a3cd078edaad6987ad5ae39baaa
EuGorDD <eugord@Yandex.ru>
2018-12-24 19:05:11 +0300

    fix: routes

--- src/router/helpers/ifNotAuthorized.js
+++ src/router/helpers/ifNotAuthorized.js
@@ -2,7 +2,7 @@ import store from '../../store';
 
 export default (to, from, next) => {
 	if (!store.getters['isLoggedIn']) {
-		return next('/login');		
+		return next('/login');
 	}
 
 	return next();
